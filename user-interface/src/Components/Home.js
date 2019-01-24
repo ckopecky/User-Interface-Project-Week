98da4d678b2bd444afa60a868a55ff1b644703e8
ckopecky <ckopecky@github.com>
2019-01-24 10:28:56 -0800

    move img folder to src so I can use them

--- user-interface/src/Components/Home.js
+++ user-interface/src/Components/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+import hamburgerMenu from "../img/nav-hamburger.png"
 class Home extends Component {
     render() {
         return (
-            <div>
-                Home
+            <div className="home-header">
+                <div className="home-header-nav" >
+                    <div className="logo">S & J</div>
+                    <img src={hamburgerMenu} alt="hamburger menu" />
+                </div>
+                <div className="slug-line">
+                    Integrity,<br/>
+					Excellence,<br/>
+					Progress.
+                </div>
             </div>
         );
     }
